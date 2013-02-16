39d28b0ef3aeb41ccb2d75fad62ce6548f972877
Rafael Cossovan <navossoc@gmail.com>
2013-02-16 11:16:10 -0200

    Changed minimum platform to Windows XP. Changed build release to use MFC in a static lib.

--- MenuTools/targetver.h
+++ MenuTools/targetver.h
@@ -5,4 +5,8 @@
 // If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
+// Support Windows XP
+#include <WinSDKVer.h>
+#define _WIN32_WINNT	_WIN32_WINNT_WINXP
+
 #include <SDKDDKVer.h>
