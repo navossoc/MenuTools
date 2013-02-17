35da97bd27236007ed3943107df84accdfa80def
Rafael Cossovan <navossoc@gmail.com>
2013-02-17 01:09:48 -0300

    Added MenuCommon folder containing sources shared by exe and dll.

---
+++ MenuCommon/Defines.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define MT_DLL_NAME32	_T("MenuToolsHook.dll")
+#define MT_DLL_NAME64	_T("MenuToolsHook64.dll")
+#define MT_EXE_NAME32	_T("MenuTools.exe")
+#define MT_EXE_NAME64	_T("MenuTools64.exe")
+#define MT_JOB_NAME		_T("MenuToolsJob")
