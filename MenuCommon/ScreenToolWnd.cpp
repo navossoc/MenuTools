41f9c4160c238a8aa745a1e8de3e2f30f2e42dbe
Simon Reber <sr.dev@outlook.com>
2021-12-08 22:30:08 +0100

    tool window works better

--- MenuCommon/ScreenToolWnd.cpp
+++ MenuCommon/ScreenToolWnd.cpp
@@ -2,7 +2,9 @@
 #include "ScreenToolWnd.h"
 
 #include <string>
-
+#include <vector>
+#include <ranges>
+#include <algorithm>
 
 struct ScreenToolWnd::Impl
 {
@@ -32,7 +34,68 @@ BOOL ScreenToolWnd::IsScreenToolWnd(HWND hWnd)
 	static wchar_t className[100] = { L'\0' };
 	GetClassName(hWnd, className, 99);
 	return CLASS_NAME.compare(className) == 0;
+} 
+
+BOOL Monitorenumproc(HMONITOR hMon, HDC hDC, LPRECT pRECT, LPARAM lParam)
+{
+	std::vector<RECT>& mInfos = *(std::vector<RECT>*)lParam;
+	//MONITORINFO mi = { 0 };
+	//GetMonitorInfo(hMon, &mi);
+	mInfos.push_back(*pRECT);
+	return TRUE;
 }
+
+LRESULT CALLBACK ToolWndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	LRESULT res = 0;
+	switch (message)
+	{
+	case WM_LBUTTONDOWN:
+		DestroyWindow(hWnd);
+		break;
+
+	case WM_MOUSEMOVE:
+	{
+		RECT r;
+		GetClientRect(hWnd, &r);
+		InvalidateRect(hWnd, &r, FALSE);
+		break;
+	}
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Add any drawing code that uses hdc here...
+		RECT r;
+		GetClientRect(hWnd, &r);
+		InflateRect(&r, -2, -2);
+
+		POINT pt;
+		GetCursorPos(&pt);
+		ScreenToClient(hWnd, &pt);
+
+		RECT quart = {r.left, r.top, (r.right - r.left) / 2 - 1, (r.bottom - r.top) / 2 - 1 };
+		
+		FillRect(hdc, &quart,GetSysColorBrush(PtInRect(&quart, pt) ? COLOR_ACTIVECAPTION : COLOR_INACTIVECAPTION));
+
+		OffsetRect(&quart, (r.right - r.left) / 2 + 2, 0);
+		FillRect(hdc, &quart,GetSysColorBrush(PtInRect(&quart, pt) ? COLOR_ACTIVECAPTION : COLOR_INACTIVECAPTION));
+
+		OffsetRect(&quart, 0, (r.bottom - r.top) / 2 + 2);
+		FillRect(hdc, &quart,GetSysColorBrush(PtInRect(&quart, pt) ? COLOR_ACTIVECAPTION : COLOR_INACTIVECAPTION));
+
+		OffsetRect(&quart, -((r.right - r.left) / 2 + 2), 0);
+		FillRect(hdc, &quart,GetSysColorBrush(PtInRect(&quart, pt) ? COLOR_ACTIVECAPTION : COLOR_INACTIVECAPTION));
+
+		EndPaint(hWnd, &ps);
+		break;
+	}
+	}
+
+	return  DefWindowProc(hWnd, message, wParam, lParam);
+}
+
 
 ScreenToolWnd::Impl::Impl(HINSTANCE hInst, HWND hParent, UINT message, WPARAM wParam, LPARAM lParam)
 	:_hWnd(nullptr)
@@ -44,11 +107,37 @@ ScreenToolWnd::Impl::Impl(HINSTANCE hInst, HWND hParent, UINT message, WPARAM wP
 		wcl.hCursor = LoadCursor(NULL, IDC_ARROW);
 		wcl.hInstance = hInst;
 		wcl.lpszClassName = CLASS_NAME.c_str();
-		wcl.lpfnWndProc = DefWindowProc;
+		wcl.lpfnWndProc = ToolWndProc;
 
 		wndClass = &wcl;
 		RegisterClassW(wndClass);
 	}
+
+	std::vector<RECT> mInfos;
+	EnumDisplayMonitors(NULL, NULL, Monitorenumproc, (LPARAM)&mInfos);
+	
+	
+
+	RECT disp = { 0 };
+	for (RECT& r : mInfos)
+	{
+		disp.left = min( r.left, disp.left );
+		disp.top = min( r.top, disp.top );
+		disp.right = max( r.right, disp.right );
+		disp.bottom = max( r.bottom, disp.bottom );
+	}
+
+	static const LONG F = 6;
+	LONG w = (disp.right - disp.left) / F;
+	LONG h = (disp.bottom - disp.top) / F;
+
+	POINT pt;
+	GetCursorPos(&pt);
+
+	disp.left = pt.x - (w / 2);
+	disp.right = disp.left + w;
+	disp.top = pt.y;
+	disp.bottom = disp.top + h + GetSystemMetrics(SM_CYCAPTION);
 
 	_hWnd = CreateWindowEx(
 		WS_EX_TOPMOST | WS_EX_TOOLWINDOW,// Optional window styles.
@@ -58,9 +147,9 @@ ScreenToolWnd::Impl::Impl(HINSTANCE hInst, HWND hParent, UINT message, WPARAM wP
 
 		// Size and position
 		//CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		100, 100, 500, 250,
+		disp.left, disp.top, w, h,
 
-		NULL,       // Parent window    
+		hParent,       // Parent window    
 		NULL,       // Menu
 		hInst,     // Instance handle
 		NULL        // Additional application data
@@ -68,6 +157,7 @@ ScreenToolWnd::Impl::Impl(HINSTANCE hInst, HWND hParent, UINT message, WPARAM wP
 	if (_hWnd)
 	{
 		::ShowWindow(_hWnd, SW_SHOW | SW_NORMAL);
+		SetActiveWindow(_hWnd);
 	}
 }
 
--- TestApp/TestApp.cpp
+++ TestApp/TestApp.cpp
@@ -121,7 +121,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    hInst = hInstance; // Store instance handle in our global variable
 
    hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+      CW_USEDEFAULT, CW_USEDEFAULT, 400, 200, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
