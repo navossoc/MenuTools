f823837dacdcb3604be20ef2f41ded43eadf3447
Simon Reber <sr.dev@outlook.com>
2021-12-12 01:01:08 +0100

    Works!

--- MenuToolsHook/dllmain.cpp
+++ MenuToolsHook/dllmain.cpp
@@ -1,12 +1,15 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
 
+HINSTANCE hInst;                                // current instance
+
 BOOL APIENTRY DllMain(HMODULE hModule,
 	DWORD  ul_reason_for_call,
 	LPVOID lpReserved
 	)
 {
-	UNREFERENCED_PARAMETER(hModule);
+	//UNREFERENCED_PARAMETER(hModule);
+	hInst = hModule;
 	UNREFERENCED_PARAMETER(ul_reason_for_call);
 	UNREFERENCED_PARAMETER(lpReserved);
 
